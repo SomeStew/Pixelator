705ed9159423c0db06a114b1b43c6188a262babb
SomeStew <156128326+SomeStew@users.noreply.github.com>
2025-02-07 10:36:42 +0100

    pixelise cleanup

--- pixelise.py
+++ pixelise.py
@@ -9,11 +9,11 @@ from PIL import Image, ImageDraw
 import random
 
 def process_image(input_image_path, output_image_path, num_pixels):
-    # Load the image
+    # Img Load
     img = Image.open(input_image_path)
     width, height = img.size
 
-    # Select random pixels
+    # Rng pixels
     selected_pixels = []
     for _ in range(num_pixels):
         x = random.randint(0, width - 1)
@@ -21,32 +21,29 @@ def process_image(input_image_path, output_image_path, num_pixels):
         hex_color = '#{:02x}{:02x}{:02x}'.format(*img.getpixel((x, y))[:3])
         selected_pixels.append((x, y, hex_color))
 
-    # Create a completely white image
+
     white_img = Image.new('RGB', (width, height), "white")
     draw = ImageDraw.Draw(white_img)
 
-    # Create quadrilaterals centered on the selected pixels
+
     for x, y, color in selected_pixels:
-        # Generate larger random dimensions for the rectangle
         rect_width = random.randint(int(min(width, height) / (250/num_pixels)), int(min(width, height) / (150/num_pixels)))
         rect_height = random.randint(int(min(width, height) / (250/num_pixels)), int(min(width, height) / (150/num_pixels)))
         top_left = (x - rect_width // 2, y - rect_height // 2)
         bottom_right = (x + rect_width // 2, y + rect_height // 2)
         
-        # Border color
         rgb = Image.new("RGB", (1, 1), color).getpixel((0, 0))
         darker_rgb = tuple(int(c * 0.7) for c in rgb)
         darker_color = '#{:02x}{:02x}{:02x}'.format(*darker_rgb)
         
-        # Draw the rectangle with the border
         draw.rectangle([top_left, bottom_right], outline=darker_color, fill=darker_color)
 
         # Filler
-        inset = 2  # Border size
+        inset = 2 
         inner_top_left = (top_left[0] + inset, top_left[1] + inset)
         inner_bottom_right = (bottom_right[0] - inset, bottom_right[1] - inset)
         draw.rectangle([inner_top_left, inner_bottom_right], fill=color)
 
-    # Save the resulting image
+    # Save image
     white_img.save(output_image_path)
     print(f"Modified image saved at: {output_image_path}")
